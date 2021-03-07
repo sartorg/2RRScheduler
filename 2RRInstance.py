ee02d9a2c7ba7161ba61a5688e470d3c48a37231
giorgios <giorgios@SINTEFPC9658>
2021-03-07 17:46:51 +0100

    Use Python naming convention.

--- 2RRInstance.py
+++ 2RRInstance.py
@@ -1,17 +1,16 @@
 # The file handles the problem instances.
 
+import xml.etree.ElementTree as et 
+
 class Instance():
-    def __init__(self, name, dataType, contributor, year):
+    def __init__(self, name, data_type, contributor, year):
         self.name = name
-        self.dataType = dataType
+        self.data_type = data_type
         self.contributor = contributor
         self.year = year
-        self.gameMode = "P"
+        self.game_mode = "P"
         self.objective = "SC"
         self.leagues = []
         self.teams = []
         self.slots = []
         self.constraints = []
-    
-
-
