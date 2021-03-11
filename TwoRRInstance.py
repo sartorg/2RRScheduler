f7d821b269222a0ed5ca03bd3ad500cdc0c7d3b8
Giorgio Sartor <Giorgio.Sartor@sintef.no>
2021-03-11 17:30:43 +0100

    Test creating first instance.

--- TwoRRInstance.py
+++ TwoRRInstance.py
@@ -14,9 +14,22 @@ class Instance():
         self.teams = []
         self.slots = []
         self.constraints = []
+    
+    def __repr__(self):
+        str = ""
+        str += "Name: " + self.name + "\n"
+        str += "Data type: " + self.data_type + "\n"
+        str += "Contributor: " + self.contributor + "\n"
+        str += "Year: " + self.year + "\n"
+        return str
 
 def read_instance(file_name):
     tree = et.parse(file_name)
     root = tree.getroot()
     for child in root:
         print(child.tag, child.attrib)
+    for child in root[0]:
+        print(child.tag, child.attrib, child.text)
+    test = Instance(root[0][0].text, root[0][1].text, root[0][2].text, root[0][3].attrib["year"])
+    print(test)
+        
