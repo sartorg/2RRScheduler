90467412c98cf298f2837bdfd15315f5cd06497c
Giorgio Sartor <Giorgio.Sartor@sintef.no>
2021-04-17 12:42:37 +0200

    Small bug.

--- run_single.py
+++ run_single.py
@@ -3,6 +3,6 @@ from TwoRRProblem import read_instance
 from TwoRRMaster import solve_master
 
 if __name__=="__main__":
-    filename = sys.argv[0]
+    filename = sys.argv[1]
     prob = read_instance(filename)
     solve_master(filename, prob, True)
